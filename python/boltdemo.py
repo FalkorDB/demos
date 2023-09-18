6b3dafecc65736fdc78ae28f9ecd50f046d13602
Guy Korland <gkorland@gmail.com>
2023-09-18 10:29:43 +0300

    Add python examples

--- python/boltdemo.py
+++ python/boltdemo.py
@@ -1,17 +1,20 @@
 from neo4j import GraphDatabase
 
-URI = "neo4j://localhost:7687"
+URI = "bolt://localhost:7687"
 
 with GraphDatabase.driver(URI) as driver:
 
+    # Create Vector index on field description in Character
     driver.execute_query("CREATE VECTOR INDEX FOR (c:Character) ON (c.description) OPTIONS {dim:5, similarityFunction:'euclidean'}")
 
+    # Fill in the Graph with some data on books and characters
     driver.execute_query( """CREATE 
                          (:Character {name:'Bastian Balthazar Bux', description:vector32f([0.1, 0.3, 0.3, 0.4, 0.7])})-[:in]->(book1:Book {name:'The Neverending Story'}),
 						 (:Character {name:'Atreyu', description:vector32f([0.3, 0.6, 0.2, 0.1, 0.4])})-[:in]->(book1),
 						 (:Character {name:'Jareth', description:vector32f([0.1, 0.3, 0.1, 0.2, 0.9])})-[:in]->(book2:Book {name:'Labyrinth'}),
 						 (:Character {name:'Hoggle', description:vector32f([0.3, 0.2, 0.5, 0.7, 0.9])})-[:in]->(book2)""")
     
+    # Find the book with the character description that is most similar (k=1) to the user's query
     records, _, _ = driver.execute_query( """CALL db.idx.vector.query
                                          ({type:'NODE', label:'Character', attribute:'description', query:vector32f([0.1, 0.4, 0.3, 0.2, 0.7]), k:1}) 
                                          YIELD entity 
@@ -19,4 +22,4 @@ with GraphDatabase.driver(URI) as driver:
                                          RETURN b.name AS name""")
     
     for record in records:
-        print(record)
+        print(record["name"])
